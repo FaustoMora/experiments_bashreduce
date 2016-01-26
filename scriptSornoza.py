2a73b9d4f4333fda982f19ebfa3c0bde38066859
Andrés Sornoza <rolansor@hotmail.com>
2016-01-26 04:57:59 -0500

    Descubriendo los recorridos.

--- scriptSornoza.py
+++ scriptSornoza.py
@@ -2,9 +2,14 @@
 
 import re
 import sys
+from random import randint
 
+i = 0
 for line in sys.stdin:
-	nstr = re.sub(r'[?|$|.|!]',r'',line)
-	nestr = re.sub(r'[^a-zA-Z0-9 ]',r'',nstr)
-	if line != '\n' or '':
-		print nestr
+	n1 = re.sub(r'[?|$|.|!]',r'',line)
+	n2 = re.sub(r'[^a-zA-Z0-9 ]',r'',n1)
+	if n2.strip():
+		i = i + 1
+		salto = '\n'
+		n3 = 'Recorrido ' +  str(i) + ':'+ salto  + n2 + salto
+		print n3
