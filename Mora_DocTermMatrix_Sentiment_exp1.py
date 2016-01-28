e8fc3a8560b9baf128a6f984b7a2bd7266f955c7
FaustoMora <faanmora@espol.edu.ec>
2016-01-28 09:58:45 -0500

    python script para generar matriz de terminos y documentos

--- Mora_DocTermMatrix_Sentiment_exp1.py
+++ Mora_DocTermMatrix_Sentiment_exp1.py
@@ -5,19 +5,26 @@ import sys
 import re
 import os
 import pandas as pd
+import operator
 import textmining
 from nltk.stem import RegexpStemmer
 from stop_words import get_stop_words
 from nltk.corpus import stopwords
+from good_bad import Adjectives
+
 
 dictionario = {}
 more_stop_en = set(get_stop_words('english'))
 more_stop_es = set(get_stop_words('spanish'))
 stop_es = set(stopwords.words("spanish"))
 stop_en = set(stopwords.words("english"))
+adj = Adjectives()
 tdm = textmining.TermDocumentMatrix()
+
+room = 0
 #for line in var:
 for line in sys.stdin:
+	room+=1
 	line = line.replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u');
 	line = re.sub('[0-9,#,!,¡,&,.]','',line)
 	line = re.sub('[^a-zA-Z]'," ",line)
@@ -29,21 +36,15 @@ for line in sys.stdin:
 	words = [w for w in words if len(w) > 2]
 	tdm.add_doc(" ".join(words))
 
-	fileDir = os.path.dirname(os.path.realpath('__file__'))
-	good = pd.read_csv(os.path.join(fileDir, 'datasets/positive_adjectives.csv'),header=0,delimiter=",",error_bad_lines=False)
-	good = good['adjectives'].values.tolist()
 
-	bad = pd.read_csv(os.path.join(fileDir, 'datasets/negative_adjectives.csv'),header=0,delimiter=",",error_bad_lines=False)
-	bad = bad['adjectives'].values.tolist()
-
-	good_count = [ words.count(ad) for ad in good if ad in words]
+	good_count = [ words.count(ad) for ad in adj.good if ad in words]
 	good_count = len(good_count)
 
-	bad_count = [ words.count(ad) for ad in bad if ad in words]
+	bad_count = [ words.count(ad) for ad in adj.bad if ad in words]
 	bad_count = len(bad_count)
 
 
-	print 'Comentario 1: Sentiment Score: '+str(good_count - bad_count)+'\n' 
+	print 'Comentario '+str(room)+': Sentiment Score: '+str(good_count - bad_count)+'\n' 
 
 
 for row in tdm.rows(cutoff=1):
