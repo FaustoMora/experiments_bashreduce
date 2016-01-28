98a4e4e8e9986d1087ed1b71173210aee7e54cd3
FaustoMora <faanmora@espol.edu.ec>
2016-01-28 09:59:40 -0500

    python script para obtener terms frequency

--- Mora_DocTermMatrix_Sentiment_exp2.py
+++ Mora_DocTermMatrix_Sentiment_exp2.py
@@ -5,18 +5,23 @@ import sys
 import re
 import os
 import pandas as pd
-import textmining
+import operator
 from nltk.stem import RegexpStemmer
 from stop_words import get_stop_words
 from nltk.corpus import stopwords
+from good_bad import Adjectives
 
 dictionario = {}
 more_stop_en = set(get_stop_words('english'))
 more_stop_es = set(get_stop_words('spanish'))
 stop_es = set(stopwords.words("spanish"))
 stop_en = set(stopwords.words("english"))
+adj = Adjectives()
+
+room = 0
 #for line in var:
 for line in sys.stdin:
+	room+=1
 	line = line.replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u');
 	line = re.sub('[0-9,#,!,¡,&,.]','',line)
 	line = re.sub('[^a-zA-Z]'," ",line)
@@ -28,14 +33,6 @@ for line in sys.stdin:
 	words = [w for w in words if len(w) > 2]
 
 
-	fileDir = os.path.dirname(os.path.realpath('__file__'))
-	good = pd.read_csv(os.path.join(fileDir, 'datasets/positive_adjectives.csv'),header=0,delimiter=",",error_bad_lines=False)
-	good = good['adjectives'].values.tolist()
-
-	bad = pd.read_csv(os.path.join(fileDir, 'datasets/negative_adjectives.csv'),header=0,delimiter=",",error_bad_lines=False)
-	bad = bad['adjectives'].values.tolist()
-
-
 	for i in words:
 		if i not in dictionario:
 			dictionario[i]=1
@@ -44,17 +41,18 @@ for line in sys.stdin:
 			dictionario.update({i:a+1})
 
 
-	good_count = [ words.count(ad) for ad in good if ad in words]
+	good_count = [ words.count(ad) for ad in adj.good if ad in words]
 	good_count = len(good_count)
 
-	bad_count = [ words.count(ad) for ad in bad if ad in words]
+	bad_count = [ words.count(ad) for ad in adj.bad if ad in words]
 	bad_count = len(bad_count)
 
 
-	print 'Comentario 1: Sentiment Score: '+str(good_count - bad_count)+'\n' 
+	print 'Comentario '+str(room)+': Sentiment Score: '+str(good_count - bad_count)+'\n'
 
-	
-for key,value in dictionario.items():
+
+dictionario = sorted(dictionario.items(), key=operator.itemgetter(0))	
+for key,value in dictionario:
 	print str(key)+','+str(value)
 
 		
